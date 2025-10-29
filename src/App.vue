cd87be287dabc7cbd2e804c5de8f468e455a89fb
AndreiSchalinski <andreischalinski@hotmail.com>
2025-10-29 12:41:41 -0300

    Implementação datatable tabela

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-form v-model="valid" style="width: 100%;">
+  <v-container fluid><v-form v-model="valid" style="width: 100%;">
     <v-container fluid>
       <v-row>
         <v-col>
@@ -7,18 +7,42 @@
         </v-col>
       </v-row>
     </v-container>
-    {{ tabela }}
+
   </v-form>
+
+  <v-data-table
+    :headers="headers"
+    :items="tabela"
+  ></v-data-table>
+  </v-container>
 </template>
 <script setup>
 import axios from '../src/plugins/axios'; 
 import { ref } from 'vue';
 
+const headers = ref([
+    {
+      title: 'Data competência',
+      align: 'start',
+      sortable: false,
+      key: 'dataInicial',
+    },
+    // { title: 'Valor de empréstimo', key: 'calories' },
+    // { title: 'Saldo devedor', key: 'fat' },
+    // { title: 'Consolidada', key: 'carbs' },
+    // { title: 'Total', key: 'protein' },
+    // { title: 'Amortização', key: 'iron' },
+    // { title: 'Saldo', key: 'iron' },
+    // { title: 'Provisão', key: 'iron' },
+    // { title: 'Acumulado', key: 'iron' },
+    // { title: 'Pago', key: 'iron' },
+  ])
+
 const tabela = ref([]);
 const form = ref({dataInicial:"2024-01-01", dataFinal:"2034-01-01", primeiroPagamento:"2024-02-15", valorEmprestimo:"140000.00", taxaJuros:0.07});
 
 const enviarDados = async () => {
-  tabela.value = await axios.post(`/api/calculadora/calcular`,form.value).then(resp => resp);
+  tabela.value = await axios.post(`/api/calculadora/calcular`,form.value).then(resp => resp.data);
 }
 
 </script>
